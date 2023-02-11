4c36a6e1d9d99a9f70365a05f9cc2edc60d16b6a
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-11 12:02:57 +0100

    fin funcion

--- Ejercicio 8/8.1.py
+++ Ejercicio 8/8.1.py
@@ -1,6 +1,7 @@
 print("Escriba cual es el precio que quiere poner a su producto")
 precio = input(int())
-print("el precio de su producto sin impuestos es de: ", precio"€")
-def(precio_con_impuestos):
+print("el precio de su producto sin impuestoes es de", precio , "euros")
+def precio_con_impuestos():
     precio_con_impuestos = precio * 1.21
-print("el precio de su producto con impuestos es de: ", precio_con_impuestos"€")
+print("el precio de su producto con impuestos es de", precio_con_impuestos , "euros")
+    
