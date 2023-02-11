275f140de797a84133c2ed3a2df0ba54116e2c39
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-11 12:09:06 +0100

    f

--- Ejercicio 8/8.1.py
+++ Ejercicio 8/8.1.py
@@ -1,10 +1,11 @@
 print("Escriba cual es el precio que quiere poner a su producto")
 precio = input(int())
 print("el precio de su producto sin impuestos es de", precio , "euros")
+@f1
 def precio_con_impuestos():
     precio_con_impuestos = precio * 1.21
     
-def funcion_decoradora(funcion_parametro):
+def f1(funcion_parametro):
     def funcion_interior():
         print("Vamos a calcular el precio de su producto con impuestos, aproximando que se añade un impuesto del 21%")
         funcion_parametro()
