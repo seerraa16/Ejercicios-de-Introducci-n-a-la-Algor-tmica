a071d003f965492f8f4fb2ec881f84dd6f8f8fda
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-11 12:07:22 +0100

    decorador

--- Ejercicio 8/8.1.py
+++ Ejercicio 8/8.1.py
@@ -1,7 +1,12 @@
 print("Escriba cual es el precio que quiere poner a su producto")
 precio = input(int())
-print("el precio de su producto sin impuestoes es de", precio , "euros")
+print("el precio de su producto sin impuestos es de", precio , "euros")
 def precio_con_impuestos():
     precio_con_impuestos = precio * 1.21
-print("el precio de su producto con impuestos es de", precio_con_impuestos , "euros")
     
+def funcion_decoradora(funcion_parametro):
+    def funcion_interior():
+        print("Vamos a calcular el precio de su producto con impuestos, aproximando que se añade un impuesto del 21%")
+        funcion_parametro()
+        print("El precio de su producto con impuestos es de", precio_con_impuestos , "euros")
+    return funcion_interior
