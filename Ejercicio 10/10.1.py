93c3843adb9ceced31984b1b7373310b6775f12e
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-11 12:55:19 +0100

    area triangulo con base y altura

---
+++ Ejercicio 10/10.1.py
@@ -0,0 +1,5 @@
+print("Vamos a calcular el area de un triangulo, para ello neceitaremos la base y la altura, introduzca primero la base y despues la altura")
+base = float(input("Introduzca la base: "))
+altura = float(input("Introduzca la altura: "))
+area = (base * altura) / 2
+print("El area del triangulo compuesto por los datos dados es: ", area)
