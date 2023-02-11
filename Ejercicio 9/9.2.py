8304a2bc9be73f80a1b843a89a9fc47b69a6c7d7
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-11 12:41:07 +0100

    Media ponderada sin decoradores

--- Ejercicio 9/9.2.py
+++ Ejercicio 9/9.2.py
@@ -1,17 +1,11 @@
-print("Escoga un numero, pulse enter, despues elija su ponderacion y pulse enter hasta que haya escogido 3 numeros. La suma de su ponderacion no puede ser mayor que 100")
+print("Escoga un numero, pulse enter, despues elija su ponderacion y pulse enter hasta que haya escogido 3 numeros y 3 ponderaciones. La suma de su ponderacion no puede ser mayor que 100")
 numero1 = int(input())
 ponderacion1 = int(input())
 numero2 = int(input())
 ponderacion2 = int(input())
 numero3 = int(input())
 ponderacion3 = int(input())
-def f1(funcion_parametro):
-    def funcion_interior():
-        print("Ahora, calcularemos la media ponderada de estos tres")
-        funcion_parametro()
-        print("La media ponderada de los 3 numeros que ha escogido es", media_ponderada)
-    return funcion_interior
-@f1
+print("Vamos a calcular la media ponderada de estos 3 numeros")
 def media_ponderada():
-    media_ponderada = (numero1*ponderacion1+numero2*ponderacion2+numero3*ponderacion3)/(ponderacion1+ponderacion2+ponderacion3)
-    return media_ponderada
+    media_ponderada = ((numero1*ponderacion1)+(numero2*ponderacion2)+(numero3*ponderacion3))/(ponderacion1+ponderacion2+ponderacion3)
+print("La media ponderada de los numeros que ha escogido es", media_ponderada)
