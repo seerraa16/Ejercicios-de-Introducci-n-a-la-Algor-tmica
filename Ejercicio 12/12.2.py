cbb29bf7a02f47fc68c0e4a641a93db541205fa2
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-12 19:40:04 +0100

    Transferencias

--- Ejercicio 12/12.2.py
+++ Ejercicio 12/12.2.py
@@ -18,5 +18,14 @@ class cuenta_bancaria:
             self.saldo -= cantidad
             print("Se ha retirado", cantidad, "€ de la cuenta de", self.nombre)
             print("El saldo actual es de", self.saldo, "€")
+    def transferir(self, cantidad, cuenta):
+        if cantidad > self.saldo:
+            print("No se puede transferir esa cantidad")
+        else:
+            self.saldo -= cantidad
+            cuenta.saldo += cantidad
+            print("Se ha transferido", cantidad, "€ de la cuenta de", self.nombre, "a la cuenta de", cuenta.nombre)
+            print("El saldo actual de", self.nombre, "es de", self.saldo, "€")
+            print("El saldo actual de", cuenta.nombre, "es de", cuenta.saldo, "€")
     
     
