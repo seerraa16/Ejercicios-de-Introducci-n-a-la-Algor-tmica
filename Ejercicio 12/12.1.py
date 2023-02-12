e7705fa687fbd16ca7452a84aa694dec32fa6fb7
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-12 19:29:17 +0100

    parte uno

---
+++ Ejercicio 12/12.1.py
@@ -0,0 +1,6 @@
+def __init__(self, usuario, dni, numero de cuenta, saldo):
+    self.usuario = usuario
+    self.dni = dni
+    self.numero de cuenta = numero de cuenta
+    self.saldo = saldo
+    
