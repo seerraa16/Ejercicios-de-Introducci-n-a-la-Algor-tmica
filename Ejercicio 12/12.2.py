233f812b92e6950213042a422b551acf728fac97
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-12 19:36:38 +0100

    copiamos 12.1 en 12.2

---
+++ Ejercicio 12/12.2.py
@@ -0,0 +1,7 @@
+class cuenta_bancaria:
+    def __init__(self, nombre, dni, IBAN, contraseña, saldo):
+        self.nombre = nombre
+        self.dni = dni
+        self.IBAN = IBAN
+        self.contraseña = contraseña
+        self.saldo = saldo
