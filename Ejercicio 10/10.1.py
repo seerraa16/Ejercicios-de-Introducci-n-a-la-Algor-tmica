cb30da00d2885a4a56601b940c481e5c72d58fbd
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-12 20:08:52 +0100

    funcion decoradora

--- Ejercicio 10/10.1.py
+++ Ejercicio 10/10.1.py
@@ -2,4 +2,12 @@ print("Vamos a calcular el area de un triangulo, para ello neceitaremos la base 
 base = float(input("Introduzca la base: "))
 altura = float(input("Introduzca la altura: "))
 area = (base * altura) / 2
-print("El area del triangulo compuesto por los datos dados es: ", area)
+def funcion_decoradora(funcion_parametro):
+    def funcion_interior():
+        print("El area del triangulo es: ", area)
+    return funcion_interior
+@funcion_decoradora
+def area_triangulo():
+    pass
+area_triangulo()
+
