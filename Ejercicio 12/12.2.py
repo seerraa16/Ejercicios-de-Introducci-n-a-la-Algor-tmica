abfb68f4f2a62d181f528a3e250d58d1cc7a6b76
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-12 19:39:36 +0100

    ingresar retirar

--- Ejercicio 12/12.2.py
+++ Ejercicio 12/12.2.py
@@ -5,3 +5,18 @@ class cuenta_bancaria:
         self.IBAN = IBAN
         self.contraseña = contraseña
         self.saldo = saldo
+    
+    #OPERACIONES
+    def ingresar(self, cantidad):
+        self.saldo += cantidad
+        print("Se ha ingresado", cantidad, "€ en la cuenta de", self.nombre)
+        print("El saldo actual es de", self.saldo, "€")
+    def retirar(self, cantidad):
+        if cantidad > self.saldo:
+            print("No se puede retirar esa cantidad")
+        else:
+            self.saldo -= cantidad
+            print("Se ha retirado", cantidad, "€ de la cuenta de", self.nombre)
+            print("El saldo actual es de", self.saldo, "€")
+    
+    
